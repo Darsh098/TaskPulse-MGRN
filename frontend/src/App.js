79c3c7e5f73240ce48fb1f00498b214ba22dc764
Darsh <darshdoshi01@gmail.com>
2024-04-16 10:59:38 +0530

    Implemented Create Task, View Task, Share Task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,106 @@
-import React from 'react';
-import CreateTaskForm from './components/CreateTaskFrom';
-import TaskList from './components/TaskList';
-import UpdateTaskForm from './components/UpdateTaskForm'
+import React, { useEffect, useState } from "react";
+import CreateTaskForm from "./components/CreateTaskFrom";
+import TaskList from "./components/TaskList";
+import UpdateTaskForm from "./components/UpdateTaskForm";
+import Navbar from "./components/Navbar";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation, useQuery } from "@apollo/client";
+import client from "./client";
+import {
+  CREATE_USER,
+  CREATE_TASK,
+  CREATE_SHARED_TASK,
+} from "./graphql/mutation";
+import { GET_TASKS_BY_USER } from "./graphql/queries";
 
 function App() {
+  const { isSignedIn, user } = useUser();
+  const [currentUser, setCurrentUser] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [createUser] = useMutation(CREATE_USER, { client });
+  const [createTask] = useMutation(CREATE_TASK, { client });
+  const [createSharedTask] = useMutation(CREATE_SHARED_TASK, { client });
 
-  
-  const tasks = [
-    { id: 1, title: 'Task 1', description: 'Description for Task 1' },
-    { id: 2, title: 'Task 2', description: 'Description for Task 2' },
-    { id: 3, title: 'Task 3', description: 'Description for Task 3' },
-  ];
+  useEffect(() => {
+    const registerUser = async (user) => {
+      try {
+        const { id, firstName, lastName, primaryEmailAddress } = user;
+        const { data } = await createUser({
+          variables: {
+            googleId: id,
+            displayName: `${firstName} ${lastName}`,
+            email: primaryEmailAddress.emailAddress,
+          },
+        });
+        setCurrentUser(data.createUser);
+      } catch (error) {
+        console.error("Error registering user:", error);
+      }
+    };
 
-  const handleCreateTask = (title, description) => {
-    console.log('Creating task:', { title, description });
+    if (isSignedIn && user) {
+      registerUser(user);
+    }
+  }, [user, isSignedIn, createUser]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (currentUser) {
+        const {
+          loading: tasksLoading,
+          error: tasksError,
+          data: tasksData,
+        } = await client.query({
+          query: GET_TASKS_BY_USER,
+          variables: { userId: currentUser.id },
+        });
+
+        if (!tasksLoading && tasksData) {
+          setTasks(tasksData.getTasksByUser);
+        }
+      }
+    };
+
+    fetchData();
+  }, [currentUser]);
+
+  if (!user) {
+    return null;
+  }
+
+  const handleCreateTask = async (title, description, emails) => {
+    try {
+      const { data } = await createTask({
+        variables: {
+          title: title,
+          description: description,
+          authorId: currentUser?.id,
+        },
+      });
+
+      if (data && data.createTask) {
+        setTasks([...tasks, data.createTask]);
+        for (const email of emails) {
+          await createSharedTask({
+            variables: {
+              taskId: data.createTask.id,
+              sharedWithUserEmail: email,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.error("Error Creating Task:", error);
+    }
   };
 
   const handleUpdateTask = (taskId, title, description) => {
-    console.log('Updating task:', taskId, title, description);
+    console.log("Updating task:", taskId, title, description);
   };
 
-  
   return (
     <div>
+      <Navbar />
       <CreateTaskForm onCreate={handleCreateTask} />
       {/* <UpdateTaskForm task={tasks[0]} onUpdate={handleUpdateTask} /> */}
       <TaskList tasks={tasks} />
